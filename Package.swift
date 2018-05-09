d602c65354823372d8f84840737441d57d05c912
Mihaela MJ <mihaelamj@me.com>
2018-05-09 20:57:57 +0200

    initial commit

--- Package.swift
+++ Package.swift
@@ -2,18 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "MakeVaporSpices",
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
+  name: "MakeVaporSpices",
+  dependencies: [
+    //Vapor
+    .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+    
+    //DB Drivers
+    .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
+    .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.2"),
+    .package(url: "https://github.com/vapor/fluent-mysql.git", from: "3.0.0-rc.2.3"),
+    
+    //VaporSpices
+    .package(url: "../VaporSpices", from: "0.0.1")
     ],
-    targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
-    ]
+  targets: [
+    .target(name: "App", dependencies: ["FluentSQLite", "FluentMySQL", "FluentPostgreSQL", "Vapor", "VaporSpices"]),
+    .target(name: "Run", dependencies: ["App"]),
+    .testTarget(name: "AppTests", dependencies: ["App"])
+  ]
 )
 
